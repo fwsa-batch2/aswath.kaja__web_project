70864586ae6af3158f7315ab85d620fbc0111a9c
ASWATH48 <kingaswath46@gmail.com>
2021-12-13 17:36:44 +0530

    updated

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,33 @@
-alert("Welcome to rcomp");
+let userList = [];
+
+function onPageload() {
+    let details = JSON.parse(localStorage.getItem("user"));
+    if (details){
+    userList = details;
+}
+}
+
+
+function loginfun() {
+    event.preventDefault();
+
+    const userName = document.getElementById("name").value.trim();
+    const address = document.getElementById("address").value.trim();
+    const phonenumber = document.getElementById("phoneNumber").value.trim();
+    const problemIn = document.getElementById("problemIn").value.trim();
+    const problemNote = document.getElementById("problemNote").value.trim();
+
+    const customerdetails = {
+        "name": userName,
+        "address": address,
+        "phonenumber": phonenumber,
+        "problemIn": problemIn,
+        "problemNote": problemNote,
+    }
+    userList.push(customerdetails);
+    let userArray = JSON.stringify(userList)
+    localStorage.setItem("user", userArray);
+  
+}
+onPageload();
+
---
+++ assets/javascript/register.js
@@ -0,0 +1,56 @@
+let userList = [];
+
+function onPageload() {
+    let details = JSON.parse(localStorage.getItem("users"));
+    if (details == null) {
+        userList =[];
+    }
+    else{
+        userList = details;
+    }
+}
+function loginfun() {
+    event.preventDefault();
+
+    let userName = document.getElementById("username").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let password = document.getElementById("password").value.trim();
+    let password2 = document.getElementById("password2").value.trim();
+
+
+
+        const customerdetails = {
+            "username": userName,
+            "email": email,
+            "password": password,
+            "password2": password2,
+        } 
+        const emailexists = emailvalid(email);
+        if(emailexists){
+            alert("email already exists")
+        }
+        else if(password != password2){
+            alert("Password mismatched")
+        }
+        else{
+            userList.push(customerdetails);
+            let userArray = JSON.stringify(userList)
+            localStorage.setItem("users", userArray);
+        }
+
+    }
+
+function emailvalid(currentemail) {
+    let userData = JSON.parse(localStorage.getItem("users"));
+    let used = false;
+    for (i = 0; i < userData.length; i++) {
+        const email = userData[i].email;
+        if (currentemail == email) {
+            used = true;
+            break;
+        }
+    }
+    return used;
+}
+
+onPageload();
