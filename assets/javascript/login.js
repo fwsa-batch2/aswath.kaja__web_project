fd85b16e22ea6651a7930295e9b0810eabccb9a7
ASWATH48 <kingaswath46@gmail.com>
2021-12-15 10:53:19 +0530

    updated files

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -2,8 +2,21 @@ let userName = document.getElementById("username");
 let password = document.getElementById("password");
   
 function userExists(){
-    let isuserExists = false;
+    
 
 
 
 }
+function emailvalid(currentemail) {
+    // let userData = JSON.parse(localStorage.getItem("users"));
+    let used = false;
+    for (i = 0; i < userList.length; i++) {
+        const email = userList[i].email;
+        if (currentemail == email) {
+            used = true;
+            break;
+        }
+    }
+    return used;
+}
+
