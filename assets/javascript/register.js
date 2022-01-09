a670a02faa0592a46b794a37cb07faf6f219b621
ASWATH48 <kingaswath46@gmail.com>
2022-01-09 22:28:31 +0530

    updated files

--- assets/javascript/register.js
+++ assets/javascript/register.js
@@ -1,29 +1,19 @@
 let userList = [];
 
 function onPageload() {
-    console.group("onPageload")
     let details = JSON.parse(localStorage.getItem("users"));
-    console.log("details")
     if (details != null) {
         userList = details;
 }
-console.groupEnd(onPageload)}
+}
 
-function register() {
+function register(event){
     event.preventDefault();
-    console.group("register")
 
     let userName = document.getElementById("username").value.trim();
-    console.log(userName)
     let email = document.getElementById("email").value.trim();
-    console.log(email)
     let password = document.getElementById("password").value.trim();
-    console.log(password)
-    let password2 = document.getElementById("password2").value.trim();
-    console.log(password2)
-
-
-
+    let password2 = document.getElementById("password2").value.trim(); 
 
         const customerdetails = {
             "username": userName,
@@ -33,21 +23,21 @@ function register() {
         } 
         console.table(customerdetails)
         const emailexists = emailvalid(email);
-        console.log(emailexists)
         if(emailexists){
             alert("email already exists")
         }
         else if(password != password2){
             alert("Password mismatched")
         }
+        else if(password===""&&password2===""){
+            alert("password cannot be empty")
+        }
         else{
             userList.push(customerdetails);
             let userArray = JSON.stringify(userList)
-            console.log(userArray)
             localStorage.setItem("users", userArray);
             window.location.href = "./../../pages/login.html";
         }
-    console.groupEnd("register")
     }
 
 function emailvalid(currentemail) {
@@ -55,7 +45,6 @@ function emailvalid(currentemail) {
     let used = false;
     for (i = 0; i < userList.length; i++) {
         const email = userList[i].email;
-        console.log(email)
         if (currentemail == email) {
             used = true;
             break;
---
+++ assets/javascript/review.js
@@ -0,0 +1,56 @@
+let reviewData = [];
+function onPageload(){
+  let exDatas = JSON.parse(localStorage.getItem("reviews"));
+  if(exDatas != null){
+    reviewData = exDatas;
+  }
+  renderDatas()
+}
+function updateDatas(reviewSource){
+  reviewData.push(reviewSource);
+  let reviewDataInString = JSON.stringify(reviewData);
+  localStorage.setItem("reviews", reviewDataInString);
+}
+function onSumbit() {
+  event.preventDefault();
+  let inputData1 = document.getElementById("userName").value;
+  //  console.log(inputData1);
+  let inputData2 = document.getElementById("userInput").value;
+  //  console.log(inputData2);
+  let exDatas = JSON.parse(localStorage.getItem("reviews"));
+  let match = false;
+  for(let i=0; i<exDatas.length; i++){    
+  if(exDatas[i].userName==inputData1){
+    match = true;
+    break;
+  }
+  }
+if(!match){ 
+  let reviewSource = {
+  userName: inputData1,
+  userData: inputData2,
+};
+updateDatas(reviewSource)
+renderDatas();
+}
+else if(match){
+  alert("Your review has been already submitted")
+}
+}
+function renderDatas() {
+  let answer ="";
+  for (let i = 0; i < reviewData.length; i++) {
+    let len = reviewData[i];
+    let nameA = len.userName;
+    console.log(nameA);
+    let review = len.userData;
+    console.log(review);
+    let temp ="<li>"+"<img src=../assets/images/profile.png class=profile_image>"+"<img src=../assets/images/reveiwstar.png class=star_img>"+"<p class = username>"+ nameA +"</p>"+"" +"<br>" +"<br>" +review+ "</li>";
+    answer = temp + answer;
+    let ulTag = document.getElementById("list");
+    ulTag.innerHTML = answer;
+      }
+}
+onPageload()
+
+
